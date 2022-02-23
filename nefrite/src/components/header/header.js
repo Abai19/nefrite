4fd30c18d82b3e061634889927f068b92acd1308
Abai <Karagulov1900@gmail.com>
2022-02-23 19:01:16 +0600

    second commit

--- nefrite/src/components/header/header.js
+++ nefrite/src/components/header/header.js
@@ -1,5 +1,10 @@
 import  styles from './header.module.css'
 import whats from '../../img/whats.png'
+import logo from '../../img/logo.png'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Header = () => {
     return (
         <>
@@ -9,6 +14,36 @@ const Header = () => {
                         <button className={styles.btn}>  +8 800 849 480 33</button>
                 </div>
                <tr/>
+               <div className={styles.containerSearch}>
+               <img src={logo} className={styles.logoBlock}/>
+               <div className={styles.searchBlock}>
+                    
+                    <input className={styles.searchInput} placeholder="Поиск" />
+                    <button className={styles.searchIcon}>
+                    <FontAwesomeIcon icon={faSearch} class={styles.searchIconFa}/>
+                        </button> 
+                    
+                        <button className={styles.shoppingCartBtn}>    <FontAwesomeIcon icon={faCartShopping} 
+                        className={styles.shoppingIconFa} />
+                        0
+                        </button>
+                </div>
+               
+               </div>
+               <nav className={styles.menuBlock}>
+                   <ul className={styles.menuUl}>
+                       <ul>
+                       <li>Категории</li>
+                       </ul>
+                       <li>Главная</li>
+                       <li>Цветы</li>
+                       <li>Новогодние</li>
+                       <li>Летние товары</li>
+                       <li>Разное</li>
+                       <li>Доставка</li>
+                       <li>Контакты</li>
+                   </ul>
+               </nav>
 
             </div>
         </>
