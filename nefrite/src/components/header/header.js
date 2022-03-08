cabd673999810e46c76389655ef215e998d4fbef
Abai <Karagulov1900@gmail.com>
2022-03-08 23:46:31 +0600

    afafaf

--- nefrite/src/components/header/header.js
+++ nefrite/src/components/header/header.js
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
     return (
         <>
-            <div className={styles.container}>
+            <div>
                 <div className={styles.container_content}>
                      <img src={whats}/>
                         <button className={styles.btn}>  +8 800 849 480 33</button>
