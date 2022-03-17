a4dd227065b4e5bd25eef1f12198b16d5ed6d9d0
Abai <Karagulov1900@gmail.com>
2022-03-17 21:29:39 +0600

    fixed header

--- nefrite/src/components/header/header.js
+++ nefrite/src/components/header/header.js
@@ -1,11 +1,10 @@
 import styles from './header.module.css'
 import React, {useState} from 'react'
-import whats from '../../img/whats.png'
 import logo from '../../img/logo.png'
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {MenuOutlined, Close} from "@material-ui/icons";
+import {MenuOutlined, Close, WhatsApp, Instagram} from "@material-ui/icons";
 import {Link} from 'react-router-dom'
 
 const Header = () => {
@@ -17,7 +16,8 @@ const Header = () => {
         <>
              <div>
                 <div className={styles.container_content}>
-                     <img src={whats}/>
+                <Instagram className={styles.instaIcon}/>
+                     <WhatsApp className={styles.whatsIcon}/>
                         <button className={styles.btn}>  +8 800 849 480 33</button>
                 </div>
                <tr/>
@@ -48,6 +48,8 @@ const Header = () => {
                    <div className={styles.closed}>
                        <Close onClick={showMenu} className={styles.close}/>
                    </div>
+                   
+                 
                    <li>
                        <Link to='/'>
                             Главная
@@ -57,21 +59,65 @@ const Header = () => {
                        <Link to='/'>
                             Цветы
                        </Link>
+                       <ul className={styles.submenu}>
+                           <li><a>Декоративные</a></li>
+                           <hr/>
+                           <li><a>Ритуальные</a> </li>
+                           <hr/>
+                           <li> <a>Праздничные</a> </li>
+                           <hr/>
+                           <li> <a>Дерево</a></li>
+                           <hr/>
+                           <li><a>Листья</a></li>
+                       </ul>
                    </li>
                    <li>
                        <Link to='/'>
                             Новогодние
                        </Link>
+                       <ul className={styles.submenu}>
+                           <li><a>Ёлки</a></li>
+                           <hr/>
+                           <li><a>Игрушки</a> </li>
+                           <hr/>
+                           <li> <a>Водопад</a> </li>
+                           <hr/>
+                           <li> <a>Дождик</a></li>
+                           <hr/>
+                           <li><a>Хвойные ветки</a></li>
+                       </ul>
                    </li>
                    <li>
                        <Link to='/'>
                             Летние товары
                        </Link>
+                       <ul className={styles.submenu}>
+                           <li><a>Бассейн</a></li>
+                           <hr/>
+                           <li><a>Болоны</a> </li>
+                           <hr/>
+                           <li> <a>Очки</a> </li>
+                           <hr/>
+                           <li> <a>Шапки</a></li>
+                          
+                       </ul>
                    </li>
                    <li>
                        <Link to='/'>
                             Разное
                        </Link>
+                       <ul className={styles.submenu}>
+                           <li><a>Электро-самокат</a></li>
+                           <hr/>
+                           <li><a>Самокат</a> </li>
+                           <hr/>
+                           <li> <a>Героскутер</a> </li>
+                           <hr/>
+                           <li> <a>Мячи</a></li>
+                           <hr/>
+                           <li><a>Ролики</a></li>
+                           <li><a>Скейтборд</a></li>
+                       </ul>
                    </li>
                    <li>
                        <Link to='/'>
